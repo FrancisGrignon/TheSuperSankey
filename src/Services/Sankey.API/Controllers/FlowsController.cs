f2fc12e6f2798fb5849e229fc64ce149b313b7fd
Francis Grignon <live.com@ncis.ca>
2018-04-13 15:15:10 -0400

    Add Dockerfiles

--- src/Services/Sankey.API/Controllers/FlowsController.cs
+++ src/Services/Sankey.API/Controllers/FlowsController.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Options;
 using Sankey.API.ViewModels;
 using Sankey.Infrastructure;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,7 +20,7 @@ namespace Sankey.API.Controllers
             _context = context;
             _settings = settings.Value;
 
-            context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            _context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
         }
 
         // GET api/flows
@@ -33,15 +32,22 @@ namespace Sankey.API.Controllers
 
         // GET api/flows/ca-qc/2015/all/fr
         [HttpGet("{geo}/{year:int}/{tag?}/{language?}")]
-        public async Task<FlowViewModel> Get(string geo, int year, string tag="root", string language = "en")
+        public async Task<FlowViewModel> Get(string geo, int year, string tag = "root", string language = "en")
         {
             var location = await _context.Geos.Where(p => p.Iso3166.Equals(geo)).SingleOrDefaultAsync();
-            var flows = await _context.Flows.Include(p => p.Source).Include(p => p.Target).Where(p => p.GeoId.Equals(location.Id) && p.Year.Equals(year) && p.Tag.Equals(tag)).ToArrayAsync();
+            var flows = await _context.Flows.Include(p => p.Source).Include(p => p.Target).Where(p => p.Geo.Iso3166.Equals(geo) && p.Year.Equals(year) && p.Tag.Equals(tag)).ToArrayAsync();
             var model = new FlowViewModel
             {
-                Geo = location.Name(language),
+                Geo = location?.Name(language),
                 Year = year,
-                Data = flows.Select(p => new FlowItemViewModel { Source = p.Source.Name(language), SourceId = p.Source.Id, Target = p.Target.Name(language), TargetId = p.TargetId, Value = p.Value }).ToArray()
+                Data = flows.Select(p => new FlowItemViewModel
+                {
+                    Source = p.Source.Name(language),
+                    SourceId = p.Source.Id,
+                    Target = p.Target.Name(language),
+                    TargetId = p.TargetId,
+                    Value = p.Value
+                }).ToArray()
             };
 
             return model;
